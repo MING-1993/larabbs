9e57180113e012246bf9d935967e8eb47e279182
vagrant <vagrant@homestead>
2021-02-12 11:25:09 +0000

    修复 XSS 注入漏洞

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -58,7 +58,7 @@
 @endsection
 
 
-@section('styles')
+{{-- @section('styles')
   <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
 @stop
 
@@ -69,20 +69,20 @@
   <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
 
   <script>
-    $(document).ready(function() {
-      var editor = new Simditor({
-        textarea: $('#editor'),
-        upload: {
-          url: '{{ route('topics.upload_image') }}',
-          params: {
-            _token: '{{ csrf_token() }}'
+      $(document).ready(function() {
+        var editor = new Simditor({
+          textarea: $('#editor'),
+          upload: {
+            url: '{{ route('topics.upload_image') }}',
+            params: {
+              _token: '{{ csrf_token() }}'
+            },
+            fileKey: 'upload_file',
+            connectionCount: 3,
+            leaveConfirm: '文件上传中，关闭此页面将取消上传。'
           },
-          fileKey: 'upload_file',
-          connectionCount: 3,
-          leaveConfirm: '文件上传中，关闭此页面将取消上传。'
-        },
-        pasteImage: true,
+          pasteImage: true,
+        });
       });
-    });
-  </script>
-@stop
+    </script>
+@stop --}}
